b4389ba5fcde3636d536cde5f4b80d3ede2f5409
Wes Bourke <wesbourkeavl@gmail.com>
2022-03-01 12:03:20 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const input1 = document.getElementById('new-task-description')
+  //console.log(input1)
+
+  const form = document.querySelector('#create-task-form')
+  console.log(form)
+
+  const divList = document.getElementById('list')
+  console.log(divList);
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    //console.log(e)
+
+    const textValue = input1.value;
+
+
+    let li = document.createElement(`li`)
+    divList.append(li);
+    console.log(divList)
+
+    li.append(textValue)
+    
+  });
+
+
 });
